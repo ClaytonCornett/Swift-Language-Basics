6ed8249a5febbd71241bda0108f1a5ed04383754
Clayton Cornett <claytoncornett@claytons-mbp.attlocal.net>
2017-06-12 19:44:53 -0500

    Created

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Clayton Cornett on 6/12/17.
+//  Copyright © 2017 Clayton Cornett. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
